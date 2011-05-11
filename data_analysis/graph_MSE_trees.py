f7bfbf1100f47570adb3487b65995d3b9f3793a8
singhala <singhala@mit.edu>
2011-05-11 02:27:28 -0400

    fly data

--- data_analysis/graph_MSE_trees.py
+++ data_analysis/graph_MSE_trees.py
@@ -11,4 +11,6 @@ err_nums = [[float(s) for s in errs] for errs in err_strings]
 MSEs = numpy.array([sum(errs)/len(errs) for errs in err_nums])
 x = numpy.arange(0, trees, 1)
 pylab.plot(x, MSEs)
+pylab.xlabel('Number of trees')
+pylab.ylabel('Average MSE between true and predicted expr')
 pylab.show()
